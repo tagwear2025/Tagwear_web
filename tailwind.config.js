0a1f6fb085d4a237f1f32ff6a08f3118f69b6f09
victor30700 <gsaldana709@gmail.com>
2025-07-17 12:46:57 -0400

    PRIMERA VERSION OF

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+import { Config } from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 const config = {
-  // Aquí está la clave: le decimos a Tailwind que escanee todos estos archivos.
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  // Habilitamos el modo oscuro para que funcione con la clase 'dark' en el <html>
   darkMode: 'class',
   theme: {
     extend: {
